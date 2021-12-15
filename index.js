61267b4505d8074cfb6674714c235502740e8367
Tharik79 <88718950+Tharik79@users.noreply.github.com>
2021-12-15 13:20:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let countdown= 11;
+
+document.querySelector(".greeting").innerText=10;
+
+setTimeout(() => {
+  document.querySelector(".greeting").innerText = 9;
+  setTimeout(() => {
+  document.querySelector(".greeting").innerText = 8;
+    setTimeout(() => {
+  document.querySelector(".greeting").innerText = 7;
+      setTimeout(() => {
+  document.querySelector(".greeting").innerText = 6;
+        setTimeout(() => {
+  document.querySelector(".greeting").innerText = 5;
+          setTimeout(() => {
+  document.querySelector(".greeting").innerText = 4;
+            setTimeout(() => {
+  document.querySelector(".greeting").innerText = 3;
+              setTimeout(() => {
+  document.querySelector(".greeting").innerText = 2;
+                setTimeout(() => {
+  document.querySelector(".greeting").innerText = 1;
+                  setTimeout(() => {
+  document.querySelector(".greeting").innerText = "Happy Independence Day !!! 🎉🎈💃👯‍";
+  }, 1000)
+  }, 1000)
+  }, 1000)
+  }, 1000)
+  }, 1000)
+  }, 1000)
+  }, 1000)
+  }, 1000)
+  }, 1000)
+  }, 1000)
